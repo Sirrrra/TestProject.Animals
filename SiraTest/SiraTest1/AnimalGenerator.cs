3dd8c2dce35e31fe6ca34d54d372950423e4e1d5
Sirrrra <33802790+Sirrrra@users.noreply.github.com>
2021-12-13 01:09:27 +0300

    Static classes, randomizer, initialization, lists.

---
+++ SiraTest/SiraTest1/AnimalGenerator.cs
@@ -0,0 +1,31 @@
+﻿using SiraTest1.Animals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiraTest1
+{
+    public class AnimalGenerator
+    {
+        public static List<Type> animals => new List<Type>
+        {
+            typeof(Cat),
+            typeof(Dog),
+            typeof(Fish)
+        };
+
+        public static List<Animal> Perform(int count)
+        {
+            var result = new List<Animal>();
+            var randomizer = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                var animalIndex = randomizer.Next(0, 3);
+                var tempAnimal = Activator.CreateInstance(animals[animalIndex]);
+                result.Add(tempAnimal as Animal);
+            }
+
+            return result;
+        }
+    }
+}
--- SiraTest/SiraTest1/Program.cs
+++ SiraTest/SiraTest1/Program.cs
@@ -9,10 +9,19 @@ namespace SiraTest1
         {
             Console.WriteLine("Hello! Its animal creator program!\n");
 
-            var dog1 = new Dog("Pes1");
-            dog1.Play();
-            dog1.Name = "Pes2";
-            dog1.Head.Eat("chicken");
+            //var dog1 = new Dog("Pes1");
+            //dog1.Play();
+            //dog1.Name = "Pes2";
+            //dog1.Head.Eat("chicken");
+
+            var zoo = AnimalGenerator.Perform(100);
+
+            zoo.ForEach(iterationObject => {
+                if (iterationObject is PlayableAnimal)
+                {
+                    (iterationObject as PlayableAnimal).Play();
+                }
+            });
         }
     }
 }
